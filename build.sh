7a2927a6449536d89c7078dfe0398d76a68d9929
Joshua Kaldon <jkaldon@lenistech.com>
2022-02-27 10:37:35 -0600

    added preStop to container lifecycle to cleanly shutdown.  opened up private network access

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-DOCKER_TAG=22.0.0-11
+DOCKER_TAG=22.0.0-12
 
 docker build --progress plain -t "jkaldon/arm64v8-bitcoind:${DOCKER_TAG}" .
 docker push "jkaldon/arm64v8-bitcoind:${DOCKER_TAG}"
